f7c5e4d60ba069bfb1eaad3f7e093cc1f4cdb997
Sander Venema <sander@sandervenema.ch>
2023-07-16 13:40:58 +0200

    Made trailing slash optional on admin & /openletter

--- wedf_openletter/urls.py
+++ wedf_openletter/urls.py
@@ -4,9 +4,9 @@ from django.contrib import admin
 
 urlpatterns = [
     re_path(r'openletter/i18n/', include('django.conf.urls.i18n')),
-    re_path(r'openletter/admin/', admin.site.urls),
+    re_path(r'openletter/admin/?', admin.site.urls),
 ]
 
 urlpatterns += i18n_patterns(
-    re_path(r'openletter/', include('petitions.urls')),
+    re_path(r'openletter/?', include('petitions.urls')),
 )
