5f79e24a416aeb16c58bbf3ebcae97860083bbb0
Sander Venema <sander@sandervenema.ch>
2023-07-16 13:33:47 +0200

    Made trailing slashes in URLs optional

--- petitions/urls.py
+++ petitions/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
     #re_path(r'^(?P<lang>[a-z]{2})?$', views.index, name='index'),
     re_path(r'^$', views.index, name='index'),
-    re_path(r'^sign/$', views.sign, name='sign'),
-    re_path(r'^confirm/([0-9a-z]{64})/$', views.confirm, name='confirm'),
-    re_path(r'^suggest/$', views.suggest, name='suggest'),
+    re_path(r'^sign/?$', views.sign, name='sign'),
+    re_path(r'^confirm/([0-9a-z]{64})/?$', views.confirm, name='confirm'),
+    re_path(r'^suggest/?$', views.suggest, name='suggest'),
 ]
